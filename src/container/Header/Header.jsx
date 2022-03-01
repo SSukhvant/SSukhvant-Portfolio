d49bed014b1d3de675cb638d1239c20711f29e92
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2022-03-02 01:04:18 +0530

    Working on Home Section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Header.scss';
+import {motion} from 'framer-motion';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='app__header'>
+    <div className='app__header-intro'>
+    
+    </div>
+    
+    <div className='app__header-pic'>
+    
+    </div>
+    </div>
   )
 }
 
